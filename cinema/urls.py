656a09de6751ca041c509d4fa837777f7791e1f9
Tomasz Herman <tomekh2015@gmail.com>
2025-10-28 13:06:52 +0100

    PEP8 fixes: line splitting, double quotes, code style

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,11 +1,17 @@
 from rest_framework.routers import DefaultRouter
-from .views import GenreViewSet, ActorViewSet, CinemaHallViewSet, MovieViewSet, MovieSessionViewSet
+from .views import (
+    GenreViewSet,
+    ActorViewSet,
+    CinemaHallViewSet,
+    MovieViewSet,
+    MovieSessionViewSet,
+)
 
 router = DefaultRouter()
-router.register(r'genres', GenreViewSet)
-router.register(r'actors', ActorViewSet)
-router.register(r'cinema_halls', CinemaHallViewSet)
-router.register(r'movies', MovieViewSet)
-router.register(r'movie_sessions', MovieSessionViewSet)
+router.register(r"genres", GenreViewSet)
+router.register(r"actors", ActorViewSet)
+router.register(r"cinema_halls", CinemaHallViewSet)
+router.register(r"movies", MovieViewSet)
+router.register(r"movie_sessions", MovieSessionViewSet)
 
 urlpatterns = router.urls
